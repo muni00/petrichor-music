16d80b1404761d20ae4843eba44cd71abc51c1f5
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2021-11-20 00:08:58 +0300

    yenilikler
    hatalarının düzeltilmiş ali

--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/IntroductoryActivity.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/IntroductoryActivity.kt
@@ -1,8 +1,12 @@
 package com.muazzeznihalbahadir.muzikoneriuygulamasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.muazzeznihalbahadir.muzikoneriuygulamasi.adapter.LoginAdapter
 import com.muazzeznihalbahadir.muzikoneriuygulamasi.databinding.ActivityIntroductoryBinding
+
 
 
 class IntroductoryActivity : AppCompatActivity() {
@@ -11,6 +15,7 @@ class IntroductoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_introductory)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         binding = ActivityIntroductoryBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -22,5 +27,16 @@ class IntroductoryActivity : AppCompatActivity() {
         binding.bgImage.animate().translationY(translationValueIki.toFloat()).setDuration(1000).setStartDelay(4000)
 
 
+        val adapter = LoginAdapter(supportFragmentManager)
+        adapter.addFragment(LoginTabFragment(),"giris")
+        //Adapter'ımızdaki verileri viewPager adapter'a veriyoruz
+        binding.viewPager.adapter = adapter
+        //Tablar arasında yani viewPager'lar arasında geçisi sağlıyoruz
+        binding.lgTabLayout.setupWithViewPager(binding.viewPager)
+
+       /* val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)*/
+
     }
+
 }
--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/LoginActivity.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/LoginActivity.kt
@@ -2,10 +2,31 @@ package com.muazzeznihalbahadir.muzikoneriuygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import com.muazzeznihalbahadir.muzikoneriuygulamasi.adapter.LoginAdapter
+import com.muazzeznihalbahadir.muzikoneriuygulamasi.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+       // setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+        //ViewPager için adapter nesnesi oluşturuyoruz ve kullanacağımız fragment, title'ları ekliyoruz.
+        val adapter = LoginAdapter(supportFragmentManager)
+        adapter.addFragment(LoginTabFragment(),"giris")
+        adapter.addFragment(SignupTabFragment(),"kayıt ol")
+        //Adapter'ımızdaki verileri viewPager adapter'a veriyoruz
+        binding.lgViewPager.adapter = adapter
+        //Tablar arasında yani viewPager'lar arasında geçisi sağlıyoruz
+        binding.tabLayout.setupWithViewPager(binding.lgViewPager)
+
+
     }
 }
