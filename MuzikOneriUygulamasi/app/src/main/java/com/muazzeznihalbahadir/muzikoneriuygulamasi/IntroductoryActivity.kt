471cd17c0f03246d448ebe5226aedac0587b5b56
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2021-11-21 13:00:32 +0300

    görüntü düzenlemesi
    hatalar giderildi arayüz düzenlendi

--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/IntroductoryActivity.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/IntroductoryActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import com.muazzeznihalbahadir.muzikoneriuygulamasi.adapter.LoginAdapter
 import com.muazzeznihalbahadir.muzikoneriuygulamasi.databinding.ActivityIntroductoryBinding
-
+import com.muazzeznihalbahadir.muzikoneriuygulamasi.databinding.FragmentTransitionTabBinding
 
 
 class IntroductoryActivity : AppCompatActivity() {
@@ -14,28 +14,26 @@ class IntroductoryActivity : AppCompatActivity() {
     private lateinit var binding : ActivityIntroductoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_introductory)
+        //tabbarın gösterilmemesindeki ayarı gerçekleştirebilmemize yarım eder
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         binding = ActivityIntroductoryBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         val translationValue = -1600
-        val translationValueIki = -1800
+        val translationValueIki = -2000
         binding.lottie.animate().translationY(translationValue.toFloat()).setDuration(1000).setStartDelay(4000)
         binding.appName.animate().translationY(translationValueIki.toFloat()).setDuration(1000).setStartDelay(4000)
-        binding.logo.animate().translationY(translationValueIki.toFloat()).setDuration(1000).setStartDelay(4000)
         binding.bgImage.animate().translationY(translationValueIki.toFloat()).setDuration(1000).setStartDelay(4000)
 
 
         val adapter = LoginAdapter(supportFragmentManager)
-        adapter.addFragment(LoginTabFragment(),"giris")
+        adapter.addFragment(TransitionTabFragment(),"welcome to pethricor")
         //Adapter'ımızdaki verileri viewPager adapter'a veriyoruz
         binding.viewPager.adapter = adapter
         //Tablar arasında yani viewPager'lar arasında geçisi sağlıyoruz
         binding.lgTabLayout.setupWithViewPager(binding.viewPager)
 
-       /* val intent = Intent(this,LoginActivity::class.java)
-        startActivity(intent)*/
+
 
     }
 
--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/LoginActivity.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.muazzeznihalbahadir.muzikoneriuygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.tabs.TabLayout
 import com.muazzeznihalbahadir.muzikoneriuygulamasi.adapter.LoginAdapter
 import com.muazzeznihalbahadir.muzikoneriuygulamasi.databinding.ActivityLoginBinding
 
