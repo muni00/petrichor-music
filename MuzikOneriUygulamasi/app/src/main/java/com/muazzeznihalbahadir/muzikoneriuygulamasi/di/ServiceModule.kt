dcbb55991b54c6a7b9604482ad6ed7e4955394ae
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2021-12-22 16:16:38 +0300

    son yenilikler
    hatalar kontrol edilirken düzenlemesi imkansız bir hal aldığından ,geri dönüş için yedekleme adına bir commit'tir,her şey güzel olacak

--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/di/ServiceModule.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/di/ServiceModule.kt
@@ -7,6 +7,7 @@ import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.muazzeznihalbahadir.muzikoneriuygulamasi.data.remote.MusicDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,6 +18,12 @@ import dagger.hilt.android.scopes.ServiceScoped
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
+
+
 
     @SuppressLint("WrongConstant")
     @ServiceScoped
--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/other/Constants.kt
+++ MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/other/Constants.kt
@@ -1,5 +1,8 @@
 package com.muazzeznihalbahadir.muzikoneriuygulamasi.other
 
 object Constants {
-    const val SONG_COLLECTION = "song"
+    const val SONG_COLLECTION = "songs"
+    const val MEDIA_ROOT_ID = "root_id"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
