89d32c3bb5a2489864873ff67f112d3585b2d563
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2022-11-29 22:49:03 +0300

    en son haliyle
    uygulamanın temelindeki hatalı proje kaldırıldı. Daha güzeli gelene kadar bu idare eder.

--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/PetrichorMusicApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.muazzeznihalbahadir.muzikoneriuygulamasi
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PetrichorMusicApplication : Application() {
-}
--- MuzikOneriUygulamasi/app/src/main/java/com/muazzeznihalbahadir/muzikoneriuygulamasi/other/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.muazzeznihalbahadir.muzikoneriuygulamasi.other
-
-object Constants {
-    const val SONG_COLLECTION = "songs"
-    const val MEDIA_ROOT_ID = "root_id"
-    const val NOTIFICATION_CHANNEL_ID = "music"
-    const val NOTIFICATION_ID = 1
-}
